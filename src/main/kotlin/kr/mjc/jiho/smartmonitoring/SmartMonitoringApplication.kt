1f47f700ea632528b11a6c72891479abdd3cb4c7
정지호 <jeongjiho@jeongjihoui-MacBookAir.local>
2025-04-16 16:32:58 +0900

    first commit

---
+++ src/main/kotlin/kr/mjc/jiho/smartmonitoring/SmartMonitoringApplication.kt
@@ -0,0 +1,11 @@
+package kr.mjc.jiho.smartmonitoring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartMonitoringApplication
+
+fun main(args: Array<String>) {
+    runApplication<SmartMonitoringApplication>(*args)
+}
---
+++ src/test/kotlin/kr/mjc/jiho/smartmonitoring/SmartMonitoringApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.mjc.jiho.smartmonitoring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartMonitoringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
